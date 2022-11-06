333b0f8574dc014d358c3b355dd5625ebc2b74b4
Melloman <lovelyboyy@mail.ru>
2022-11-06 12:09:06 +0300

    i

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileWriter file = new FileWriter("file.txt");
+        file.write("""
+                Aa
+                Bb
+                Cc
+                Dd
+                Ee
+                Ff
+                Gg
+                Hh
+                Ii
+                Jj
+                Kk
+                Ll
+                Mm
+                Nn
+                Oo
+                Pp
+                Qq
+                Rr
+                Ss
+                Tt
+                Uu
+                Vv
+                Ww
+                Xx
+                Yy
+                Zz
+                0
+                1
+                2
+                3
+                4
+                5
+                6
+                7
+                8
+                9
+                """);
+        file.close();
+        FileReader reader = new FileReader("file.txt");
+        Scanner sc = new Scanner(reader);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
     }
 }
